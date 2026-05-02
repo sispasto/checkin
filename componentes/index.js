58c85bc86dc076dd12a66cec749256a5f023ae6b
sispasto <sispasto@envia.co>
2026-05-02 16:33:06 -0500

    Add welcome.js import to index.js

--- componentes/index.js
+++ componentes/index.js
@@ -1,7 +1,7 @@
-import "./welcome.js";
 import "./acercade.js";
 import "./generarToken.js";
 import "./registrarServidor.js";
 import "./registrarTerminal.js";
 import "./menuTemplates.js";
 import "./marcarAsistencia.js";
+import "./welcome.js";
